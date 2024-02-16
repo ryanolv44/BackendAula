3a8ba7bc9bdb3047835ee2386b505f7ea0922c71
Ryan <franciscoryan012@gmail.com>
2024-02-16 16:12:27 -0300

    atualização

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,27 +1,28 @@
-import express from "express"
+import express from 'express'
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json({message: 'essa rota é /objeto/objeto'})
+    res.json({message: 'Esta é a rota /product/'})
 })
 
-router.get('/:id', (req, res) =>{
+router.get('/:id', (req, res) => {
     const id = req.params.id
-    res.json({message: 'Esta rota é a rota /user/:id '+'ID = '+id})
+    res.json({message: 'Esta é a rota /product/:id '+'ID = '+id})
 })
 
-router.post('/', (req, res) =>{
-    res.json({message: 'essa é a rota POST /user'})
+router.post('/', (req, res) => {
+    const user = req.body
+    res.json({message: 'Esta é a rota POST /product/', user})
 })
 
-router.put('/', (req, res) =>{
-    res.json({message: 'essa é a rota PUT /user'})
+router.put('/', (req, res) => {
+    res.json({message: 'Esta é a rota PUT /product/'})
 })
 
-router.delete('/:id', (req, res) =>{
+router.delete('/:id', (req, res) => {
     const id = req.params.id
-    res.json({message: 'Esta rota é a rota DELETE /user/:id '+'ID = '+id})
+    res.json({message: 'Esta é a rota DELETE /product/:id '+'ID = '+id})
 })
 
 export default router
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,31 +1,29 @@
-import express from "express"
+import express from 'express'
 
 const router = express.Router()
 
-//rotas do user /user
-//rotas oo get /user/
-
 router.get('/', (req, res) => {
-    res.json({message: 'essa rota é /produto'})
-})
-
-router.get('/:id', (req, res) =>{
-    const id = req.params.id
-    res.json({message: 'Esta rota é a rota /produto/:id '+'ID = '+id})
-})
-
-router.post('/', (req, res) =>{
     const user = req.body
-    res.json({message: 'essa é a rota POST /user'})
+    res.json({message: 'Esta é a rota GET /user/', user})
 })
 
-router.put('/', (req, res) =>{
-    res.json({message: 'essa é a rota PUT /user'})
-})
-
-router.delete('/:id', (req, res) =>{
+router.get('/:id', (req, res) => {
     const id = req.params.id
-    res.json({message: 'Esta rota é a rota DELETE /user/:id '+'ID = '+id})
+    res.json({message: 'Esta é a rota /user/:id '+'ID = '+id})
+})
+
+router.post('/', (req, res) => {
+    const user = req.body
+    res.json({message: 'Esta é a rota POST /user/', user})
+})
+
+router.put('/', (req, res) => {
+    res.json({message: 'Esta é a rota PUT /user/'})
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    res.json({message: 'Esta é a rota DELETE /user/:id '+'ID = '+id})
 })
 
 export default router
