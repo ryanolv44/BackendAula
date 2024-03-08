e31a22ec54b1a6003f2bbebde11de773b54314e5
Ryan <franciscoryan012@gmail.com>
2024-03-08 15:29:24 -0300

    getone

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import listAll from '../controllers/product/listAll.js'
-import getById from '../controllers/product/getById.js'
+import getById from '../controllers/product/getOne.js'
 import create from '../controllers/product/create.js'
 import update from '../controllers/product/update.js'
 import remove from '../controllers/product/remove.js'
